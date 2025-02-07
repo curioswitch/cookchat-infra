ecee5ce330daa33f3ea6f7f892e2ffebb63a2c98
Choko <choko@curioswitch.org>
2025-02-07 14:40:12 +0900

    Add initial infra for cookchat including aiplatform (#1)

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { App } from "cdktf";
+import { CookchatStack } from "./stacks/cookchat/index.js";
+import { SysadminStack } from "./stacks/sysadmin/index.js";
+
+const app = new App();
+
+new SysadminStack(app);
+
+new CookchatStack(app, {
+  environment: "dev",
+  project: "cookchat-dev",
+  domain: "alpha.cookchat.curioswitch.org",
+});
+
+new CookchatStack(app, {
+  environment: "prod",
+  project: "cookchat-prod",
+  domain: "cookchat.curioswitch.org",
+});
+
+app.synth();
