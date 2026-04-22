972554d1f31454c760e20d6e76d3bd67b3dba351
Choko <choko@curioswitch.org>
2026-04-22 12:55:08 +0900

    Apply changes (#7)

--- stacks/cookchat/gcp-services.ts
+++ stacks/cookchat/gcp-services.ts
@@ -1,4 +1,4 @@
-import { ProjectService } from "@cdktf/provider-google/lib/project-service/index.js";
+import { ProjectService } from "@cdktn/provider-google/lib/project-service/index.js";
 import { Construct } from "constructs";
 
 export interface GcpServicesConfig {
@@ -6,12 +6,30 @@ export interface GcpServicesConfig {
 }
 
 export class GcpServices extends Construct {
+  public readonly apiKeys: ProjectService;
+  public readonly cloudTasks: ProjectService;
+
   constructor(scope: Construct, config: GcpServicesConfig) {
     super(scope, "gcp-services");
 
     new ProjectService(this, "aiplatform", {
       project: config.project,
       service: "aiplatform.googleapis.com",
+    });
+
+    this.apiKeys = new ProjectService(this, "apikeys", {
+      project: config.project,
+      service: "apikeys.googleapis.com",
+    });
+
+    this.cloudTasks = new ProjectService(this, "cloud-tasks", {
+      project: config.project,
+      service: "cloudtasks.googleapis.com",
+    });
+
+    new ProjectService(this, "generativelanguage", {
+      project: config.project,
+      service: "generativelanguage.googleapis.com",
     });
   }
 }
--- stacks/sysadmin/index.ts
+++ stacks/sysadmin/index.ts
@@ -1,11 +1,13 @@
-import { GithubProvider } from "@cdktf/provider-github/lib/provider/index.js";
-import { GoogleBetaProvider } from "@cdktf/provider-google-beta/lib/provider/index.js";
-import { DataGoogleBillingAccount } from "@cdktf/provider-google/lib/data-google-billing-account/index.js";
-import { DataGoogleOrganization } from "@cdktf/provider-google/lib/data-google-organization/index.js";
-import { GoogleProvider } from "@cdktf/provider-google/lib/provider/index.js";
-import { RandomProvider } from "@cdktf/provider-random/lib/provider/index.js";
-import { Bootstrap } from "@curioswitch/cdktf-constructs";
-import { GcsBackend, TerraformStack } from "cdktf";
+import { GithubProvider } from "@cdktn/provider-github/lib/provider/index.js";
+import { DataGoogleBillingAccount } from "@cdktn/provider-google/lib/data-google-billing-account/index.js";
+import { DataGoogleOrganization } from "@cdktn/provider-google/lib/data-google-organization/index.js";
+import { DnsManagedZone } from "@cdktn/provider-google/lib/dns-managed-zone/index.js";
+import { DnsRecordSet } from "@cdktn/provider-google/lib/dns-record-set/index.js";
+import { GoogleProvider } from "@cdktn/provider-google/lib/provider/index.js";
+import { GoogleBetaProvider } from "@cdktn/provider-google-beta/lib/provider/index.js";
+import { RandomProvider } from "@cdktn/provider-random/lib/provider/index.js";
+import { Bootstrap } from "@curioswitch/cdktn-constructs";
+import { GcsBackend, TerraformStack } from "cdktn";
 import type { Construct } from "constructs";
 
 export class SysadminStack extends TerraformStack {
@@ -41,19 +43,40 @@ export class SysadminStack extends TerraformStack {
       organizationId: org.orgId,
       billingAccountId: billing.id,
       githubOrg: "curioswitch",
-      domain: "cookchat.curioswitch.org",
+      domain: "coopii.app",
       appRepositoryConfig: {
         description: "A cooking assistant",
         hasIssues: true,
         hasProjects: true,
         hasWiki: false,
-        homepageUrl: "https://cookchat.curioswitch.org",
+        homepageUrl: "https://coopii.app",
       },
       googleBeta,
     });
 
     new GcsBackend(this, {
       bucket: bootstrap.sysadminProject.tfstateBucketName,
+    });
+
+    const oldAlphaZone = new DnsManagedZone(this, "alpha-cookchat", {
+      name: "alpha-cookchat-curioswitch-org",
+      project: bootstrap.devProject.project.name,
+      dnsName: "alpha.cookchat.curioswitch.org.",
+    });
+
+    const oldProdZone = new DnsManagedZone(this, "prod-cookchat", {
+      name: "cookchat-curioswitch-org",
+      project: bootstrap.prodProject.project.name,
+      dnsName: "cookchat.curioswitch.org.",
+    });
+
+    new DnsRecordSet(this, "prod-alpha-ns-delegate", {
+      project: bootstrap.prodProject.project.name,
+      managedZone: oldProdZone.name,
+      name: "alpha.cookchat.curioswitch.org.",
+      type: "NS",
+      rrdatas: oldAlphaZone.nameServers,
+      ttl: 21600,
     });
   }
 }
